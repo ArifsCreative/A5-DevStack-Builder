b25ce0ce2be60be220fd0e746809715aa99dd918
ArifsCreative <mr.arifcontact@gmail.com>
2026-09-13 18:22:32 +0600

    Technologies

---
+++ src/components/Technologies.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Technologies = () => {
+  return (
+    <section className="py-28">
+      <div className="container mx-auto">
+        <h2 className="text-3xl font-bold text-gray-800">
+          Explore the <span className="text-pink-500">Technologies</span>
+        </h2>
+
+        <p className="mt-2 text-sm text-gray-500">
+            Pick one technology per category to build your ideal stack.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Technologies;
